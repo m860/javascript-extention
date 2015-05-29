ed06c9c6733467eeff6a9a8961484978b14f8a5f
jean <mahai_1986@126.com>
2015-05-29 15:23:00 +0800

    add locaiton.query

---
+++ src/window.location.query.js
@@ -0,0 +1,24 @@
+if (window.location.query) {
+    if (console) {
+        console.warn("window.location.query is exists.");
+    }
+}
+else {
+    function query(url) {
+        if (!url) url = window.location.href;
+        var data = {};
+        var index = url.indexOf("?");
+        var qstr = url.substring(index);
+        var strs = qstr.split("&");
+        var kv;
+        for (var i = 0; i < strs.length; i++) {
+            kv = strs[i].split("=");
+            if (kv.length > 0) {
+                data[kv[0]] = kv.length > 1 ? kv[1] : "";
+            }
+        }
+        return data;
+    }
+
+    window.location.query = query;
+}
